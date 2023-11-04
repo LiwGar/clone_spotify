b4deda19a6c1b84d137dd04ae3a286899a23d5e7
wilgar <wilenri341@hotmail.com>
2023-11-04 14:43:10 -0500

    Configuracion y preparacion inicial del proyecto.

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className=''>
+      <Head>
+        <title>Create Next App</title>
+        <meta name='description' content='Generate by create next app'/>
+        <link rel='icon' href='/'/>
+      </Head>
+      <h1 className='bg-red'>Hola Mundo</h1>
+    </div>
+  )
+}
