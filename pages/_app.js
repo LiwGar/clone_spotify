dbbe85a7de7a82846ba9eb998f8b05755f5f1143
wilgar <wilenri341@hotmail.com>
2023-11-11 22:05:11 -0500

    Se Agrega icono de Spotify en el  Head.

--- pages/_app.js
+++ pages/_app.js
@@ -11,9 +11,9 @@ function MyApp({Component, pageProps}) {
   return (
     <>
       <Head>
-          <title>Clone Spotify - Web Player:Music for everyone</title>
+          <title>Spotify - Web Player:Music for everyone</title>
           <meta name='description' content='Generate by create next app'/>
-          <link rel='icon' href='/'/>
+          <link rel='icon' href='https://cdn.freebiesupply.com/logos/large/2x/spotify-2-logo-png-transparent.png'/>
       </Head>
       <div className='bg-spotify-black min-h-screen pt-2 text-gray-400'>
         <Header setShowSidebar={setShowSidebar} />
